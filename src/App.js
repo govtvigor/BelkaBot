67a7d53eb7137b2897b46289f0d743e94a58f322
marhosha23 <marhosha23@gmail.com>
2024-08-15 19:15:23 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-// import React, { useEffect } from 'react';
-
-// function App() {
-//   useEffect(() => {
-//     if (window.Telegram.WebApp) {
-//       const tg = window.Telegram.WebApp;
-//       tg.ready();
-//     }
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <h1>Welcome to Squirrel Game!</h1>
-//       <p>This is a simple integration with Telegram Web Apps.</p>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import GameArea from './GameArea';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    if (window.Telegram.WebApp) {
+      const tg = window.Telegram.WebApp;
+      tg.ready();
+      // Вы можете добавить здесь другие взаимодействия с Telegram WebApp API
+    }
+  }, []);
+
   return (
     <div className="App">
       <GameArea />
@@ -31,4 +19,3 @@ function App() {
 }
 
 export default App;
-
