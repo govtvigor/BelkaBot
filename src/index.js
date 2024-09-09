45f5f13db68f578543beba42e0bac612379fcdd8
marhosha23 <marhosha23@gmail.com>
2024-09-09 18:17:33 +0300

    1231

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './client/App';
 import { Buffer } from 'buffer';
 
 window.Buffer = Buffer; 
